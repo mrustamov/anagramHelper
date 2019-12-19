9b5182100e401e7a45f2ad3ca9c8b5eea6a333e3
Muhammadzohir Rustamov <mrustamov@psi.de>
2019-12-19 16:32:05 +0100

    last letter not highlighted is fixed-refactoring needed

--- main.js
+++ main.js
@@ -16,6 +16,13 @@ function printtheword() {
 function printInfo() {
   document.getElementById("info").innerHTML = infoword;
 }
+/*
+function letterDoesNotExist(i) {
+  document.getElementById("inputText").style.borderColor = "red";
+  infoword += inputText.charAt(i);
+  printInfo();
+}
+*/
 
 function getRestOfLetters(str) {
   var regex = new RegExp(/(\w)(?![^<]*>|[^<>]*<\/)/, "g");
@@ -23,6 +30,7 @@ function getRestOfLetters(str) {
   if (inputword_regex) {
     return inputword_regex.join("");
   }
+  return null;
 }
 
 function highlight() {
@@ -34,52 +42,59 @@ function highlight() {
   match = 0;
   console.log("Match " + match);
   var inputText = document.getElementById("inputText").value;
+  if (inputText == 0) {
+    printInfo();
+  }
   console.log(getRestOfLetters(inputword).length);
 
   for (i = 0; i < inputText.length; i++) {
     console.log("releat until " + inputText.length + 10);
     //  && (lengthOfWord-match+1!=getRestOfLetters(inputword).length  OR && InputWordCheck.includes(inputText.charAt(i)) == true
     // after matching all the letter not more checked
-    if (getRestOfLetters(inputword).indexOf(inputText.charAt(i)) >= 0) {
-      console.log(
-        "5. Letter: " + inputText.charAt(i) + " exists in word: " + inputword
-      );
-      console.log(InputWordCheck.includes(inputText.charAt(i)));
-      match = match + 1;
-      console.log("sum " + (lengthOfWord - match));
-      console.log("Match in" + match);
-      console.log("Lengthofword in " + lengthOfWord);
-      console.log(
-        "rest of the letters: " +
-          getRestOfLetters(inputword) +
-          "  and length: " +
-          getRestOfLetters(inputword).length
-      );
-      document.getElementById("inputText").style.borderColor = "green";
-      // Replace untagged letter
-      var re = new RegExp(
-        "[" + inputText.charAt(i) + "]" + "(?![^<]*>|[^<>]*</)"
-      );
-      inputword = inputword.replace(
-        re,
-        "<span class='highlight'>" + inputText.charAt(i) + "</span>"
-      );
-      printtheword();
-      if (getRestOfLetters(inputword).length == 0) {
-        console.log("all letters are match");
-        break;
-      }
-    } else {
-      //getInputWord();
-      console.log(
-        "6. Letter: " +
-          inputText.charAt(i) +
-          " does not exists in word: " +
-          inputword
-      );
+    if (getRestOfLetters(inputword) == null) {
       document.getElementById("inputText").style.borderColor = "red";
       infoword += inputText.charAt(i);
       printInfo();
+    }
+
+    if (getRestOfLetters(inputword)!=null) {
+      if (getRestOfLetters(inputword).indexOf(inputText.charAt(i)) >= 0) {
+        console.log(
+          "5. Letter: " + inputText.charAt(i) + " exists in word: " + inputword
+        );
+        console.log(InputWordCheck.includes(inputText.charAt(i)));
+        match = match + 1;
+        console.log("sum " + (lengthOfWord - match));
+        console.log("Match in" + match);
+        console.log("Lengthofword in " + lengthOfWord);
+        console.log(
+          "rest of the letters: " +
+            getRestOfLetters(inputword) +
+            "  and length: " +
+            getRestOfLetters(inputword).length
+        );
+        document.getElementById("inputText").style.borderColor = "green";
+        // Replace untagged letter
+        var re = new RegExp(
+          "[" + inputText.charAt(i) + "]" + "(?![^<]*>|[^<>]*</)"
+        );
+        inputword = inputword.replace(
+          re,
+          "<span class='highlight'>" + inputText.charAt(i) + "</span>"
+        );
+        printtheword();
+      } else {
+        //getInputWord();
+        console.log(
+          "6. Letter: " +
+            inputText.charAt(i) +
+            " does not exists in word: " +
+            inputword
+        );
+        document.getElementById("inputText").style.borderColor = "red";
+        infoword += inputText.charAt(i);
+        printInfo();
+      }
     }
   }
 }
