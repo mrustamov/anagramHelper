f83daa93910b5ac5c5340141739229aa6153b5e7
mrustamov <rustamov.90@gmail.com>
2019-12-19 22:18:52 +0100

    redundant loggers deleter, mehtods retrieved, better logging

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 var inputword;
 var infoword = "";
-var match;
-var InputWordCheck;
 
 function getInputWord() {
   inputword = document.getElementById("inputWord").value;
-  InputWordCheck = document.getElementById("inputWord").value;
   printtheword();
 }
 
@@ -16,13 +13,18 @@ function printtheword() {
 function printInfo() {
   document.getElementById("info").innerHTML = infoword;
 }
-/*
-function letterDoesNotExist(i) {
+
+function letterDoesNotExist(inputtext, i) {
+  console.log(
+    "Letter: " +
+      inputtext.charAt(i) +
+      " does not exists in word or rest of it: " +
+      getRestOfLetters(inputword)
+  );
   document.getElementById("inputText").style.borderColor = "red";
-  infoword += inputText.charAt(i);
+  infoword += inputtext.charAt(i);
   printInfo();
 }
-*/
 
 function getRestOfLetters(str) {
   var regex = new RegExp(/(\w)(?![^<]*>|[^<>]*<\/)/, "g");
@@ -33,74 +35,46 @@ function getRestOfLetters(str) {
   return null;
 }
 
+function replace(inputtext, i) {
+  console.log(
+    "Letter: " +
+      inputtext.charAt(i) +
+      " exists in word or rest of it: " +
+      getRestOfLetters(inputword)
+  );
+  document.getElementById("inputText").style.borderColor = "green";
+  // Replace untagged letter
+  var re = new RegExp("[" + inputtext.charAt(i) + "]" + "(?![^<]*>|[^<>]*</)");
+  inputword = inputword.replace(
+    re,
+    "<span class='highlight'>" + inputtext.charAt(i) + "</span>"
+  );
+  printtheword();
+}
+
 function highlight() {
+  console.clear();
   infoword = "";
   getInputWord();
-  //debugger;
-  var lengthOfWord = document.getElementById("inputWord").value.length;
-  console.log("Lengthofword " + lengthOfWord);
-  match = 0;
-  console.log("Match " + match);
   var inputText = document.getElementById("inputText").value;
+  console.log("Length: " + inputText.length);
   if (inputText == 0) {
     printInfo();
   }
-  console.log(getRestOfLetters(inputword).length);
-
   for (i = 0; i < inputText.length; i++) {
-    console.log("releat until " + inputText.length + 10);
-    //  && (lengthOfWord-match+1!=getRestOfLetters(inputword).length  OR && InputWordCheck.includes(inputText.charAt(i)) == true
-    // after matching all the letter not more checked
     if (getRestOfLetters(inputword) == null) {
-      document.getElementById("inputText").style.borderColor = "red";
-      infoword += inputText.charAt(i);
-      printInfo();
+      letterDoesNotExist(inputText, i);
     }
-
-    if (getRestOfLetters(inputword)!=null) {
+    if (getRestOfLetters(inputword) != null) {
       if (getRestOfLetters(inputword).indexOf(inputText.charAt(i)) >= 0) {
-        console.log(
-          "5. Letter: " + inputText.charAt(i) + " exists in word: " + inputword
-        );
-        console.log(InputWordCheck.includes(inputText.charAt(i)));
-        match = match + 1;
-        console.log("sum " + (lengthOfWord - match));
-        console.log("Match in" + match);
-        console.log("Lengthofword in " + lengthOfWord);
-        console.log(
-          "rest of the letters: " +
-            getRestOfLetters(inputword) +
-            "  and length: " +
-            getRestOfLetters(inputword).length
-        );
-        document.getElementById("inputText").style.borderColor = "green";
-        // Replace untagged letter
-        var re = new RegExp(
-          "[" + inputText.charAt(i) + "]" + "(?![^<]*>|[^<>]*</)"
-        );
-        inputword = inputword.replace(
-          re,
-          "<span class='highlight'>" + inputText.charAt(i) + "</span>"
-        );
-        printtheword();
+        replace(inputText, i);
       } else {
-        //getInputWord();
-        console.log(
-          "6. Letter: " +
-            inputText.charAt(i) +
-            " does not exists in word: " +
-            inputword
-        );
-        document.getElementById("inputText").style.borderColor = "red";
-        infoword += inputText.charAt(i);
-        printInfo();
+        letterDoesNotExist(inputText, i);
       }
     }
   }
 }
 /*
-// Secpmd attampt to implement function
-
 var inputWord = document.getElementById("inputWord");
 if (inputWord) {
   inputWord.addEventListener("input", onInputProcessor);
